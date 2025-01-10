f2176475b058e85d9dc61f041638176a5b061515
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-10 16:54:40 +0100

    next

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const PORT = 5000;
 
 // Ceate a server 
 const server = http.createServer((req, res) => {
-    res.setHeader(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Welcome to my NodeJS server");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}/`);
 });
