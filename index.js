0ee76c90b843aac8b39e4ae7dd93e5b541eaa580
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-10 18:37:06 +0100

    route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Using core Node To spin server
+//Spin server with http module
 const http = require('http');
 
 //create PORT
@@ -6,8 +6,13 @@ const PORT = 5000;
 
 // Ceate a server 
 const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Welcome to my NodeJS server");
+    res.end("Welcome to my NodeJS server");}
+    else if (req.url === '/about' && req.method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('This NodeJS server was created by Moses Sam');
+    }
 });
 
 server.listen(PORT, () => {
